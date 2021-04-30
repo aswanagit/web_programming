8a2d50f964ae2f410b6982e064251d0b920fb493
ASWANA B KRISHNA <aswanabkrishna@gmail.com>
2021-04-30 16:32:12 +0530

    msg

---
+++ swapping.php
@@ -0,0 +1,15 @@
+<?php
+
+$a=67;
+$b=89;
+
+echo "the numbers before swapping is:<br>";
+echo "number a=".$a."and b=".$b;
+
+$c=$a;
+$a=$b;
+$b=$c;
+
+echo "<br><br> After swapping:<br>";
+echo "number a=".$a. "and b=".$b;
+?>
